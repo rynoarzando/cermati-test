36198e8aff1a5c5a86408d0b8736bc6b5234291a
rynoarzando <ryno.arzando@kokatto.com>
2021-04-05 13:51:46 +0700

    remove parralax and set 3 section of the main page

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,7 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="transparent"
-      elevate-on-scroll
-    >
-      <div class="d-flex align-center">
-        <v-img
-          class="mx-2"
-          src="https://i.imgur.com/TyaFz6K.png" 
-          max-height="40"
-          max-width="40"
-          contain
-        ></v-img>
-
-  <v-toolbar-title class="ml-2">
-    
-  </v-toolbar-title>
-
-        <!-- <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        /> -->
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <!-- <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn> -->
-    </v-app-bar>
-
     <v-main>
+      <toolbar-page/>
       <headerPage/>
       <contentPage/>
       <footerPage/>
@@ -50,12 +11,14 @@
 
 <script>
 import headerPage from './components/headerPage';
+import toolbarPage from './components/toolbarPage.vue';
 
 export default {
   name: 'App',
 
   components: {
-    headerPage
+    headerPage,
+    toolbarPage
   },
 
   data: () => ({
--- src/components/headerPage.vue
+++ src/components/headerPage.vue
@@ -1,8 +1,42 @@
-
-
+<template>
+  <section style="height: 100vh;">
+      <v-layout white--text align-center column justify-center style="margin-top: 8.5em;">
+        <h1 class="display-3 text-xs-center">Hello! I'm Ryno Arzando</h1>
+        <h2 class="subheading">Consult, Design, and Develop Websites</h2>
+        <h3 class="subtitle-1"> Have something great in mind? Feel free to contact me.</h3>
+        <h3 class="subtitle-1"> I'll help you make it happen.</h3>
+        <br>
+        <v-flex row wrap text-xs-center>
+          <v-btn 
+          @mouseover="isHovering = true" 
+          @mouseout="isHovering = false"
+          class="hovering">{{ isHovering ? "Woot! Hovered" : "Hover over me" }}
+            LET'S MAKE CONTACT</v-btn>
+        </v-flex>
+      </v-layout>
+    </section>
+</template>
 
 <script>
 export default {
-    name: 'indexPage',
+    name: 'headerPage',
+
+    data() {
+    return {
+      isHovering: false
+    };
+  }
 }
 </script>
+
+<style>
+.buttonHoverOff{
+  color:transparent;
+}
+.buttonHoverOn{
+  color:grey;
+}
+.hovering{
+  color: red
+}
+</style>
